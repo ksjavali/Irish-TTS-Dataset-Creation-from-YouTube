455ff0e1c6bd3765e079b649b3d6e4e74fe96abd
kritikasjavali <112214199+kritikasjavali@users.noreply.github.com>
2024-03-05 14:47:43 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Assuming other configurations remain the same
 ORGANIZED_DIR = Path('./audios_transcripts')
-DEVELOPER_KEY = 'AIzaSyBOvVwCkJKIY3RCWAUogb9Y8UbGjMN6fec'
+DEVELOPER_KEY = 'ADD_YOUR_KEY'
 AUDIO_FILES_DIR = './audios'
 TRANSCRIPTS_DIR = './transcripts'
 #ORGANIZED_DIR = './audios_transcripts'
